1e0845251bacaa0480cb7b1666d7e105f7da5d98
oscar02r <oscary.ramirez@gmail.com>
2020-05-01 22:51:24 +0200

    Creating form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import Image from './criptomonedas.jpeg';
+import Formulario from './components/Formulario';
 
+const Cotenedor = styled.div`
+      max-width: 900px;
+      margin: 0 auto;
+      @media (min-width:992px){
+             display: grid;
+             grid-template-columns: repeat(2, 1fr);
+             column-gap: 2rem;
+      }
+     
+`;
+
+const Imagen = styled.img`
+      width: 100%;
+      margin-top: 5rem;
+
+`;
+
+const Heading = styled.h1`
+      font-family: 'Bebas Neue', cursive;
+      color: #FFF;
+      text-align: left;
+      font-weight: 700;
+      font-size: 50px;
+      margin-top: 80px;
+      margin-bottom: 50px;
+
+      &::after{
+         content: '';
+         width: 100%;
+         height:5px;
+         background-color: #66A2FE;  
+         display: block;      
+      }
+`;
 
 function App() {
   return (
-    <>
-     <h1>Cripto monedas</h1> 
-    </>
+    <Cotenedor>
+      <div>
+         <Imagen
+           src ={Image}
+           alt="Imagen cripto"
+         />
+      </div>
+
+      <div>
+        <Heading>Cotiza Criptomonedas al instante</Heading>
+        <Formulario/>
+      </div>
+    </Cotenedor>
   );
 }
 
---
+++ src/hooks/useMoneda.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const useMoneda = () => {
+    const [state, actualizarState] = useState('');
+    return ( 
+       <>
+        
+       </> 
+    );
+}
+ 
+export default useMoneda;
